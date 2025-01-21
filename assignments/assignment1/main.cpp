4f72c400d30fc684733abe5d6e9232e989efc177
Alexander Bianca <alexander.bianca@mymail.champlain.edu>
2025-01-20 20:45:52 -0500

    Frame buffer stufff

---
+++ assignments/assignment1/main.cpp
@@ -0,0 +1,226 @@
+#include <stdio.h>
+#include <math.h>
+
+#include <ew/external/glad.h>
+//#include <ew/externalshader.h>
+
+#include <GLFW/glfw3.h>
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
+
+void framebufferSizeCallback(GLFWwindow* window, int width, int height);
+GLFWwindow* initWindow(const char* title, int width, int height);
+void drawUI();
+
+//Global state
+int screenWidth = 1080;
+int screenHeight = 720;
+float prevFrameTime;
+float deltaTime;
+
+// Cache
+#include <ew/shader.h>
+
+#include <ew/model.h>
+
+#include <ew/camera.h>
+#include <ew/cameraController.h>
+ew::Camera camera;
+ew::CameraController camControl;
+
+#include <ew/transform.h>
+ew::Transform suzanneTransform;
+
+#include <ew/texture.h>
+
+struct Material
+{
+	// Ambient coefficent 0-1
+	float aCoff = 1.0;
+	// Diffuse coefficent 0-1
+	float dCoff = 0.5;
+	// Specular coefficent 0-1
+	float sCoff = 0.5;
+	// Size of specular highlight
+	float shine = 125;
+} material;
+
+struct Framebuffer
+{
+	GLuint fbo;
+	GLuint color0;
+	GLuint color1;
+	GLuint depth;
+} framebuffer;
+
+void resetCam(ew::Camera* camera, ew::CameraController* camControl)
+{
+	camera->position = glm::vec3(0, 0, 5.0f);
+	camera->target = glm::vec3(0);
+	camControl->yaw = camControl->pitch = 0;
+}
+
+// render loop
+void qwerty(ew::Shader shader, ew::Model model, GLuint texture, GLint normalMap, GLFWwindow* window, float deltaTime)
+{
+	// 1. pipeline defenition
+	glEnable(GL_CULL_FACE);
+	glCullFace(GL_BACK); // Backface culling
+	glEnable(GL_DEPTH_TEST); // Depth testing
+
+	// 2. gfx pass, a pass is what is going onto the screen
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glClearColor(0.6f, 0.8f, 0.92f, 1.0f);
+
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, texture);
+
+	glActiveTexture(GL_TEXTURE1);
+	glBindTexture(GL_TEXTURE_2D, normalMap);
+
+	shader.use();
+	shader.setMat4("transform_model", glm::mat4(1.0f));
+	shader.setMat4("camera_viewproj", camera.projectionMatrix() * camera.viewMatrix());
+	shader.setVec3("eyePosition", camera.position);
+	shader.setFloat("material.aCoff", material.aCoff);
+	shader.setFloat("material.dCoff", material.dCoff);
+	shader.setFloat("material.sCoff", material.sCoff);
+	shader.setFloat("material.shine", material.shine);
+	shader.setInt("_Maintex", 0);
+	shader.setInt("_NormalMap", 1);
+
+	suzanneTransform.rotation = glm::rotate(suzanneTransform.rotation, deltaTime, glm::vec3(0.0, 1.0, 0.0));
+
+	shader.setMat4("transform_model", suzanneTransform.modelMatrix());
+
+	camControl.move(window, &camera, deltaTime);
+
+	model.draw();
+}
+
+int main() {
+	GLFWwindow* window = initWindow("Assignment 1", screenWidth, screenHeight);
+	glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
+
+	ew::Shader lit_shader = ew::Shader("assets/lit.vert", "assets/lit.frag");
+	ew::Model suzanne = ew::Model("assets/suzanne.fbx");
+
+	// Initalize camera
+	camera.position = glm::vec3( 0.0f, 0.0f, 5.0f);
+	camera.target = glm::vec3( 0.0f, 0.0f, 0.0f );
+	camera.aspectRatio = (float)screenWidth / screenHeight;
+	camera.fov = 60.0f;
+	//brick_color.jpg PavingStones138.png
+	GLuint pavingTexture = ew::loadTexture("assets/stone_wall_04_diff_4k.jpg");
+	GLuint pavingNormalMap = ew::loadTexture("assets/stone_wall_04_nor_gl_4k.jpg");
+
+	// Initalize framebuffers
+	glGenFramebuffers(1, &framebuffer.fbo);
+	glBindFramebuffer(GL_FRAMEBUFFER, framebuffer.fbo);
+
+	// Color attachment
+	glGenTextures(1, &framebuffer.color0);
+	glBindTexture(GL_TEXTURE_2D, framebuffer.color0);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 800, 600, 0, GL_RGB, GL_UNSIGNED_BYTE, nullptr);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, framebuffer.color0, 0);
+
+	// Check completeness
+	if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+	{
+		printf("Buffer not complete!");
+		return 0;
+	}
+
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+
+		float time = (float)glfwGetTime();
+		deltaTime = time - prevFrameTime;
+		prevFrameTime = time;
+
+		//RENDER
+		glClearColor(0.6f,0.8f,0.92f,1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		// This is our main render
+		qwerty(lit_shader, suzanne, pavingTexture, pavingNormalMap, window, deltaTime);
+
+		drawUI();
+
+		glfwSwapBuffers(window);
+	}
+	printf("Shutting down...");
+}
+
+void drawUI() {
+	ImGui_ImplGlfw_NewFrame();
+	ImGui_ImplOpenGL3_NewFrame();
+	ImGui::NewFrame();
+
+	ImGui::Begin("Settings");
+
+	if (ImGui::Button("Reset Camera"))
+	{
+		resetCam(&camera, &camControl);
+	}
+
+	if (ImGui::CollapsingHeader("Material")) {
+		ImGui::SliderFloat("Ambient", &material.aCoff, 0.0f, 1.0f);
+		ImGui::SliderFloat("Diffuse", &material.dCoff, 0.0f, 1.0f);
+		ImGui::SliderFloat("Specular", &material.sCoff, 0.0f, 1.0f);
+		ImGui::SliderFloat("Shine", &material.shine, 2.0f, 1024.0f);
+	}
+
+	ImGui::End();
+
+	ImGui::Render();
+	ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+}
+
+void framebufferSizeCallback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+	screenWidth = width;
+	screenHeight = height;
+}
+
+/// <summary>
+/// Initializes GLFW, GLAD, and IMGUI
+/// </summary>
+/// <param name="title">Window title</param>
+/// <param name="width">Window width</param>
+/// <param name="height">Window height</param>
+/// <returns>Returns window handle on success or null on fail</returns>
+GLFWwindow* initWindow(const char* title, int width, int height) {
+	printf("Initializing...");
+	if (!glfwInit()) {
+		printf("GLFW failed to init!");
+		return nullptr;
+	}
+
+	GLFWwindow* window = glfwCreateWindow(width, height, title, NULL, NULL);
+	if (window == NULL) {
+		printf("GLFW failed to create window");
+		return nullptr;
+	}
+	glfwMakeContextCurrent(window);
+
+	// The proc address is where the the calls are located in the specific gpu
+	if (!gladLoadGL(glfwGetProcAddress)) {
+		printf("GLAD Failed to load GL headers");
+		return nullptr;
+	}
+
+	//Initialize ImGUI
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	ImGui_ImplOpenGL3_Init();
+
+	return window;
+}
+
